a6d68053383486975b76ffac3866edbf23d02144
Piotr Gebala <piotr.gebala@fastwhitecat.com>
2020-05-14 11:52:20 +0200

    Fixed loading gtin instead sku value

--- Cron/DownloadOpinions.php
+++ Cron/DownloadOpinions.php
@@ -145,11 +145,11 @@ class DownloadOpinions
             }
 
             foreach ($opinions['items'] as $opinion) {
-                if (!isset($opinion['product']) || (!isset($opinion['product']['gtin']) && !isset($opinion['product']['local_id'])) || !isset($opinion['public_identifier'])) {
+                if (!isset($opinion['product']) || !isset($opinion['product']['local_id']) || !isset($opinion['public_identifier'])) {
                     return;
                 }
 
-                $opinion['product'] = isset($opinion['product']['gtin']) ? $opinion['product']['gtin'] : $opinion['product']['local_id'];
+                $opinion['product'] = $opinion['product']['local_id'];
 
                 try {
                     if ($this->opinionExist($opinion['public_identifier'])) {

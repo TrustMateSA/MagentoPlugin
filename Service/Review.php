4b5b7ce0769a87c596382b56ae55b170969308bd
MLupinski <michal.lupinski121@gmail.com>
2025-11-21 19:44:29 +0100

    Improve the invitation sending

--- Service/Review.php
+++ Service/Review.php
@@ -9,10 +9,17 @@ declare(strict_types=1);
 namespace TrustMate\Opinions\Service;
 
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Product;
 use Magento\Framework\Exception\AlreadyExistsException;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Locale\Resolver;
+use Magento\Framework\UrlInterface;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Model\Order;
+use Magento\Store\Model\Store as MagentoStore;
 use TrustMate\Opinions\Logger\Logger;
+use TrustMate\Opinions\Model\Category;
 use TrustMate\Opinions\Model\Config\Data;
 use TrustMate\Opinions\Model\Review as ReviewModel;
 use TrustMate\Opinions\Model\Store;
@@ -35,9 +42,19 @@ class Review
     private $productRepository;
 
     /**
+     * @var Category
+     */
+    private $category;
+
+    /**
      * @var Store
      */
     private $store;
+
+    /**
+     * @var Resolver
+     */
+    private $resolver;
 
     /**
      * @var Logger
@@ -47,21 +64,26 @@ class Review
     /**
      * @param ReviewModel $reviewModel
      * @param ProductRepositoryInterface $productRepository
+     * @param Category $category
      * @param Data $config
      * @param Store $store
+     * @param Resolver $resolver
      * @param Logger $logger
      */
     public function __construct(
-        ReviewModel                $reviewModel,
+        ReviewModel $reviewModel,
         ProductRepositoryInterface $productRepository,
-        Data                       $config,
-        Store                      $store,
-        Logger                     $logger
+        Category $category,
+        Data $config,
+        Store $store,
+        Resolver $resolver,
+        Logger $logger
     ) {
         $this->reviewModel = $reviewModel;
         $this->productRepository = $productRepository;
         $this->config = $config;
         $this->store = $store;
+        $this->resolver = $resolver;
         $this->logger = $logger;
     }
 
@@ -106,5 +128,54 @@ class Review
             'status' => $item['status'],
             'store_id' => $storeId
         ];
+    }
+
+    /**
+     * @throws NoSuchEntityException
+     */
+    public function prepareInvitationData(Order|OrderInterface $order, bool $includeProducts): array
+    {
+        $storeId = (int) $order->getStoreId();
+        $invitationData = [
+            'customer_name' => $order->getCustomerFirstname(),
+            'send_to' => $order->getCustomerEmail(),
+            'order_number' => $order->getIncrementId(),
+            'language' => strstr($this->resolver->getLocale(), '_', true),
+            'source_type' => 'magento3.0'
+        ];
+
+        if ($includeProducts) {
+            foreach ($order->getAllVisibleItems() as $item) {
+                $product = $item->getProduct();
+                $localId = $this->config->isFixLocalIdEnabled($storeId) ? $product->getId() : $product->getSku();
+                $store = $order->getStore();
+                $gtinCode = $this->config->getGtinCode($storeId);
+                $mpnCode = $this->config->getMpnCode($storeId);
+
+                $invitationData['products'][$product->getSku()] = [
+                    'id' => $localId,
+                    'name' => $product->getName(),
+                    'sku' => $product->getSku(),
+                    'product_url' => $product->getProductUrl(),
+                    'category' => $this->category->getCategoriesPath($product->getCategoryIds()),
+                    'image_url' => $this->getImageUrl($store, $product),
+                    'image_thumb_url' => $this->getImageUrl($store, $product, true),
+                    'gtin' => $gtinCode ? $product->getData($gtinCode) : null,
+                    'mpn' => $mpnCode ? $product->getData($mpnCode) : null
+                ];
+            }
+        }
+
+        return $invitationData;
+    }
+
+    private function getImageUrl(MagentoStore $store, Product $product, bool $thumbnail = false): string
+    {
+        return sprintf(
+            '%s%s%s',
+            $store->getBaseUrl(UrlInterface::URL_TYPE_MEDIA),
+            'catalog/product',
+            ($thumbnail) ? $product->getThumbnail() : $product->getImage()
+        );
     }
 }
